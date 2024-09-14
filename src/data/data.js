d0ac29dddc79d9e7ec061a62e10291f361f1cbca
Lila Amelia <lilaamelia77@gmail.com>
2024-09-14 14:09:30 +0700

    first data created

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+export const fcExhibitions = [
+    {
+        name: "SURREAL LEGACIES",
+        headline: "",
+        date: "26 September – 21 December 2024",
+        place: "Monaco",
+        desc: "Of the early 20th Century avant-gardes, few movements have had such an enduring impact on our understanding of the modern self, as well as on the visual language of modern life, as Surrealism. In the anniversary year of the founding of the Surrealist movement, and the publication in October 1924 of its first manifestos by Yvan Goll and André Breton, this exhibition will focus on the legacies of Surrealism. The influence of this movement will be explored in and through the work of major reference figures and contemporary artists in the Hauser & Wirth programme, including artists such as Ida Applebroog, Phyllida Barlow, Louise Bourgeois, Hélène Delprat, Camille Henrot, Luchita Hurtado, Cathy Josefowitz, Allison Katz, Erna Rosenstein and Anj Smith.The exhibition will focus, moreover, on the work of women artists, a strength of the gallery’s programme and within Surrealism as it developed (though the movement began as a largely male group of artists). In recent years, the importance of women within, on the periphery of, and following the Surrealist movement has been increasingly recognised. This has been identified as a transgenerational tendency, which itself proves the longer history of Surrealism and its ongoing relevance to successive generations. It connects Surrealism with feminism and later developments in contemporary art, showing this movement to have a continuing history to the present day. Additionally, it demonstrates the ongoing richness of Surrealist strategies and styles for the current moment. "
+    },
+    {
+        name: "Mark Bradford",
+        headline: "Exotica",
+        date: "26 September 2024 – 1 March 2025",
+        place: "Hong Kong",
+        desc: "Beginning 26 September, Hauser & Wirth will present ‘Exotica,’ a solo exhibition of new, formally innovative works by Mark Bradford. In an exhibition that extends the artist’s recent formal and thematic investigations while pushing his practice towards distinctly new inventions, Bradford probes the enduring impact of colonialism and concepts of ‘otherness’ through the lens of individual experience. Consisting of around 20 new paintings, ‘Exotica’ introduces a signature staining technique, wherein the artist uses caulk to create shadow-like imprints upon the canvas. These forms inject Bradford’s layered compositions with a trace of fantasy, strangeness, and memory. In its diversity of form and material – also encompassing works created with fabric dye, inked-paper, and oxidized paper – the exhibition reflects the continued evolution of Bradford’s play with figuration."
+    },
+    {
+        name: "Günther Förg",
+        headline: "Arbeiten auf Papier / Works on Paper / Oeuvres sur Papier: 1975 – 2009",
+        date: "27 September – 20 December 2024",
+        place: "Zurich, Limmatstrasse",
+        desc: "The largest survey exhibition of Günther Förg’s works on paper, spanning over 30 years, opens at Hauser & Wirth Zurich, Limmatstrasse, this September. Förg’s works on paper were an integral part of his multi-disciplinary practice (which comprised drawing, painting, photography and sculpture) and ran parallel to his works on canvas. Executed in a variety of materials, from watercolor, acrylic and oil to charcoal, chalk and ink, these creations are considered works in their own right; instead of using them as preparatory sketches for paintings, the artist would often be inspired to paint and draw on paper after experimenting with his large-scale canvases. The exhibition includes well-known series, including his Grid, Color Field, Grey and Spot works, alongside lesser-known pieces, such as early works on paper from his studies in Munich, rare monotypes, as well as his later series An die Leine and Mostly Landscapes. With many shown together for the first time, the exhibition positions Förg as a daring conceptualist who both incorporated and critiqued tropes of modernism, while celebrating his distinctively sensuous approach to gestural abstraction. His works on paper offer an insight into the way in which Förg engaged with these concerns by ceaselessly transforming his use of color, form and composition to push the boundaries of his own image making. Sidestepping easy categorization, he candidly appropriated and re-imagined canonical art historical references, such as the work of Blinky Palermo, Paul Klee, Barnett Newman, Mark Rothko, Cy Twombly, Edvard Munch and others."
+    },
+    {
+        name: "George Rouy",
+        headline: "The Bleed, Part I",
+        date: "7 October – 21 December 2024",
+        place: "London",
+        desc: "Emerging as a leading figure of the new generation of painters, George Rouy’s debut solo exhibition at Hauser & Wirth London, ‘The Bleed, Part I’ will feature a new body of work continuing his inquiry into collective mass, multiplicities and movement, and human modes of existence. The second chapter, ‘The Bleed, Part II,’ will follow at Hauser & Wirth Downtown Los Angeles in February 2025. Rouy’s dynamic and signature use of the human figure, vexed with desire, alienation and crisis, speaks to the emotional extremities of our time, resulting in explorations of identity in a globalized, technologically driven 21st Century. Amongst the themes Rouy explored in these new works is the idea of ‘carrying;’ how we are carried into life and eventually carried out of life. The works are considering collective care, how we care for each other from birth until death; how our lives are a sequence of experiences of balance and unease; feelings of being carried, or feeling the opposite—being dropped. What happens to the mass of individuals placed into surrounding space.‘The bleed’ is a term Rouy uses in relation to the surrounds of the figures; the relationship between figure and void and how those two realms interact in terms of the surface of the paintings: a physical seeping, blending and merging. ‘The surrounds’ refers to the place where the flesh and inner parts of the body meet its surrounding conditions—temperature, heat, etc. Articulating a vocabulary of figurative painting which is as distinctive as it is visceral, Rouy’s paintings are defined by contradictions—stasis and flow, precision and indeterminacy—in doing so he undermines the body as a fixed unit, proposing instead a body that constantly imagines and defines itself through its relationship with itself, with others, and with the world at large. Accompanying the exhibition, our Learning program will involve the artist himself, engaging learners with his work. This includes an ‘Educators Evening’ on Thursday 7 November."
+    },
+    {
+        name: "Jack Whitten",
+        headline: "Speedchaser",
+        date: "7 October – 21 December 2024",
+        place: "London",
+        intro: "‘The black + white paintings have forced me to be cooler, imposed a limitation upon my work habit and structure; forced me to tighten the visual concept; provided a personal framework of references plus a stamp of originality: THEY SAY WHITTEN.’—Jack Whitten, 1975",
+        desc: "Over the course of a six-decade career, Jack Whitten’s work has bridged rhythms of gestural abstraction and process art, arriving at a nuanced language of painting that hovers between mechanical automation and intensely personal expression. Focusing on Whitten’s paintings, works on paper and sculptures from the 1970s, this exhibition showcases a juncture in the artist’s career, which saw him reject the gestural brushstrokes of abstract expressionism in favor of experimental processes and materials. Displaying Whitten’s long-standing interest in craft and woodwork, the exhibition also includes carved and assembled sculptures made by the artist during the 1970s. The exhibition includes rare works from Whitten’s landmark, monochromatic Greek Alphabet series (1975 – 1978), which was the focus of a dedicated exhibition at Dia Beacon, New York NY, (2022 – 2023). The exhibition at Hauser & Wirth London goes beyond the monochrome to also display Whitten’s experimentation with color during this process-based period. In March 2025, The Museum of Modern Art (MoMA) will open ‘Jack Whitten: The Messenger,’ the first comprehensive retrospective dedicated to the groundbreaking American artist. "
+    },
+    {
+        name: "Rashid Johnson",
+        headline: "Anima",
+        date: "14 October – 21 December 2024",
+        place: "Paris",
+        desc: "Recognized as one of the leading voices of his generation, Rashid Johnson’s new works in this exhibition at our Paris gallery, which span painting, sculpture and film, demonstrate the artist’s longstanding interest in the concepts of interiority and self-reflection. The exhibition marks the gallery debut of two new bodies of painting, the closely-related Soul Paintings and God Paintings, both series that Johnson has developed over the past year. Alongside and evolving out of the works on canvas are two new series of bronze sculptures, their roughly-modeled surfaces bearing witness to the artist’s hand in a way that has dominated his sculptural practice in recent years. Also on display is the artist’s latest film, ‘Sanguine,’ exploring relationships of attention and care among three generations of the artist's family: his father, himself and his son. Continuing to expand his distinctive visual lexicon, this exhibition exemplifies the artist’s interest in animism, the belief in which all things, including inanimate objects, have souls. Through the concept of animism, the artist connects to a reality beyond the physical, building an expansive vision of the universe in which all objects, including the paintings and sculptures on view, are imbued with spiritual life."
+    }
+]
+
+export const fcEvents = [
+    {
+        
+    }
+]
